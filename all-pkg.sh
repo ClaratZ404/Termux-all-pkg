d541d643c6facb22540290f8c1d5d933574a8c80
ClaratZ404 <158544038+ClaratZ404@users.noreply.github.com>
2024-02-02 11:57:52 +0700

    Add files via upload

---
+++ all-pkg.sh
@@ -0,0 +1,27 @@
+clear
+echo
+pkg install python 
+pkg install wget
+pkg install php
+pkg install java
+pkg install python2
+pkg install python3
+pkg install curl
+pkg install perl
+pkg install bash
+pkg install nano
+pkg install openssl
+pkg install openssh
+pkg install clang
+pkg install nmap
+pkg install w3m
+pkg install hydra
+pkg install ruby
+pkg install macchanger
+pkg install host
+pkg install zip
+pkg install golang
+pkg install cowsay
+pkg install unzip
+pkg install tar
+pkg install tor
